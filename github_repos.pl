90687d8af4a8af574dede7e5d44f4096f2138757
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-09-20 12:50:43 +0200

    + about-Open-Street-Map

--- github_repos.pl
+++ github_repos.pl
@@ -55,6 +55,7 @@ $repos{'jqGrid'                      } = {url => 'https://github.com/ReneNyffene
 $repos{'jQuery'                      } = {url => 'https://github.com/ReneNyffenegger/about-jQuery'                 , dir => $about_dir };
 $repos{'jQuery-UI'                   } = {url => 'https://github.com/ReneNyffenegger/about-jQuery-UI'              , dir => $about_dir };
 $repos{'node.js'                     } = {url => 'https://github.com/ReneNyffenegger/about-node.js'                , dir => $about_dir };
+$repos{'Open-Street-Map'             } = {url => 'https://github.com/ReneNyffenegger/about-Open-Street-Map'        , dir => $about_dir };
 $repos{'perl'                        } = {url => 'https://github.com/ReneNyffenegger/about-perl'                   , dir => $about_dir };
 $repos{'php'                         } = {url => 'https://github.com/ReneNyffenegger/about-php'                    , dir => $about_dir };
 $repos{'Pig'                         } = {url => 'https://github.com/ReneNyffenegger/about-Pig'                    , dir => $about_dir };
