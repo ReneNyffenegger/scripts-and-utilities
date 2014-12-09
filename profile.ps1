f19dc9b34653c7a38f587acc91ae81b27f4b710f
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-12-09 11:30:50 +0100

    Add function dsb

--- profile.ps1
+++ profile.ps1
@@ -4,5 +4,8 @@
 #    @powershell -executionpolicy bypass -noExit -file c:\lib\Scripts\profile.ps1
 #
 
-
+# Equivalent of «dir /od» in cmd.exe
 function dod { get-childItem | sort-object lastWriteTime }
+
+# Equivalent of «dir /s /b» in cmd.exe  ( http://stackoverflow.com/a/1479683/180275 )
+function dsb { get-childItem -recurse | select-object  -expandProperty fullName }
