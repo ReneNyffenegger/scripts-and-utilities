9d902a002a68e7395e93baf10f4d4ea559ca70c5
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-10-03 13:15:54 +0200

    + about-Protocols

--- ghr.pl
+++ ghr.pl
@@ -90,6 +90,7 @@ $repos{'MapReduce'                    } = {url => 'https://github.com/ReneNyffen
 $repos{'Open-Street-Map'              } = {url => 'https://github.com/ReneNyffenegger/about-Open-Street-Map'           , dir => $about_dir };
 $repos{'perl'                         } = {url => 'https://github.com/ReneNyffenegger/about-perl'                      , dir => $about_dir };
 $repos{'php'                          } = {url => 'https://github.com/ReneNyffenegger/about-php'                       , dir => $about_dir };
+$repos{'Protocols'                    } = {url => 'https://github.com/ReneNyffenegger/about-protocols'                 , dir => $about_dir };
 $repos{'Pig'                          } = {url => 'https://github.com/ReneNyffenegger/about-Pig'                       , dir => $about_dir };
 $repos{'powershell'                   } = {url => 'https://github.com/ReneNyffenegger/about-powershell'                , dir => $about_dir };
 $repos{'python'                       } = {url => 'https://github.com/ReneNyffenegger/about-python'                    , dir => $about_dir };
