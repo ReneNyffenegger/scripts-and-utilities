ad2ec7b6812101d4754fe55fda01bcb534384b61
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-11-30 10:42:26 +0100

    Could /Z be used to remove the task?

--- notify_me_at.bat
+++ notify_me_at.bat
@@ -8,4 +8,9 @@ set message=%hour%:%minute% - %*
 
 set taskname=NotifyMeAt%year%%month%%day%%year%%hour%%minute%
 
+@rem
+@rem  TODO
+@rem    The task removes itself after running once. Maybe, /Z could
+@rem    be used to delete the task after running once.
+@rem
 @schtasks /create /SC once /ST %hour%:%minute%:%day% /TN %taskname% /SD 30/11/2015 /TR "cmd /c msg * %message% && schtasks /delete /tn %taskname% /f"
