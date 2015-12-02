d8ab91b8602cdb0a0d7f82c4d077838f445fe78d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-02 16:56:55 +0100

    + about/iMacro

--- ghr.pl
+++ ghr.pl
@@ -85,6 +85,7 @@ $repos{'Excel'                        } = {url => 'https://github.com/ReneNyffen
 $repos{'GoogleEarth'                  } = {url => 'https://github.com/ReneNyffenegger/about-GoogleEarth'               , dir => $about_dir };
 $repos{'Graphviz'                     } = {url => 'https://github.com/ReneNyffenegger/about-Graphviz'                  , dir => $about_dir };
 $repos{'html'                         } = {url => 'https://github.com/ReneNyffenegger/about-html'                      , dir => $about_dir };
+$repos{'iMacro'                       } = {url => 'https://github.com/ReneNyffenegger/about-iMacro'                    , dir => $about_dir };
 $repos{'javascript'                   } = {url => 'https://github.com/ReneNyffenegger/about-javascript'                , dir => $about_dir };
 $repos{'jqGrid'                       } = {url => 'https://github.com/ReneNyffenegger/about-jqGrid'                    , dir => $about_dir };
 $repos{'jQuery'                       } = {url => 'https://github.com/ReneNyffenegger/about-jQuery'                    , dir => $about_dir };
