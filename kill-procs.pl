0d246eefa3a14889dfee47475c2d221b26b16e72
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-01-06 07:19:20 +0100

    ID

--- kill-procs.pl
+++ kill-procs.pl
@@ -3,19 +3,36 @@ use strict;
 use Win32::Process;
 use Win32::Process::List;
 
-my $which = 'open-office';
+my $which = shift; # 'open-office';
 
+die unless $which;
 my $reg_ex;
 if ($which eq 'open-office') {
   $reg_ex = 'soffice.exe|soffice.bin|swriter.exe';
+}
+elsif ($which eq 'ID') { # Input Director
+  $reg_ex = 'InputDirectorSessionHelper.exe';
+}
+elsif ($which eq 'show') {
+}
+else {
+  die;
 }
 
 
 my $procs = new Win32::Process::List;
 my %proc_list = $procs->GetProcesses();
 foreach my $pid ( keys %proc_list ) {
-   if ($proc_list{$pid} =~ /$reg_ex/) {
-#     print "$proc_list{$pid}\n";      
-      Win32::Process::KillProcess($pid, 0) and print "Killed $proc_list{$pid} ($pid)\n";
-   }
+  if ($which eq 'show') { 
+    printf "%4d %s\n", $pid, $proc_list{$pid};      
+    next;
+  }
+  if ($proc_list{$pid} =~ /$reg_ex/) {
+     print "$proc_list{$pid} $pid\n";      
+     if (Win32::Process::KillProcess($pid, 0)) {
+       print "Killed $proc_list{$pid} ($pid)\n";   
+     } else {
+       print "For a reason $proc_list{$pid} ($pid) was not killed\n";   
+     }
+  }
 }
