3a3730eeb98ed3e5b62c070f658fad491a05ed5e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-02-10 09:46:41 +0100

    + notes

--- ghr.pl
+++ ghr.pl
@@ -133,6 +133,7 @@ $repos{'Graphic-Design-Fonts'         } = {url => 'https://github.com/ReneNyffen
 $repos{'kaggle'                       } = {url => 'https://github.com/ReneNyffenegger/kaggle'                          , dir => $github_dir};
 $repos{'Meta-Oracle'                  } = {url => 'https://github.com/ReneNyffenegger/Meta-Oracle'                     , dir => $github_dir};
 $repos{'netcat'                       } = {url => 'https://github.com/ReneNyffenegger/netcat'                          , dir => $github_dir};
+$repos{'notes'                        } = {url => 'https://github.com/ReneNyffenegger/notes'                           , dir => $github_dir};
 $repos{'oracle-patterns'              } = {url => 'https://github.com/ReneNyffenegger/oracle-patterns'                 , dir => $github_dir};
 $repos{'oracle_scriptlets'            } = {url => 'https://github.com/ReneNyffenegger/oracle_scriptlets'               , dir => $github_dir};
 $repos{'OracleTool'                   } = {url => 'https://github.com/ReneNyffenegger/OracleTool'                      , dir => $github_dir};
