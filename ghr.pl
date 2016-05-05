c2c4d4921c10da6ff8ec112c51a4e7c3513a5c2a
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-05-05 11:04:02 +0200

    + about-sed

--- ghr.pl
+++ ghr.pl
@@ -115,6 +115,7 @@ $repos{'powershell'                   } = {url => 'https://github.com/ReneNyffen
 $repos{'python'                       } = {url => 'https://github.com/ReneNyffenegger/about-python'                    , dir => $about_dir };
 $repos{'README.md'                    } = {url => 'https://github.com/ReneNyffenegger/about-README.md'                 , dir => $about_dir };
 $repos{'r'                            } = {url => 'https://github.com/ReneNyffenegger/about-r'                         , dir => $about_dir };
+$repos{'sed'                          } = {url => 'https://github.com/ReneNyffenegger/about-sed'                       , dir => $about_dir };
 $repos{'sqlite'                       } = {url => 'https://github.com/ReneNyffenegger/about-sqlite'                    , dir => $about_dir };
 $repos{'Stellarium'                   } = {url => 'https://github.com/ReneNyffenegger/about-Stellarium'                , dir => $about_dir };
 $repos{'svg'                          } = {url => 'https://github.com/ReneNyffenegger/about-svg'                       , dir => $about_dir };
