c35ef39e9beb7f3fe1062337684c073c2ea90a71
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-12-05 19:14:07 +0100

    \ /

--- pc.pl
+++ pc.pl
@@ -12,7 +12,42 @@ use warnings; use strict;
 use Cwd;
 use Win32::Clipboard;
 
+my $escape_backslashes   = 0;
+my $make_forward_slashes = 0;
+if (@ARGV) {
+
+  my $opt = shift @ARGV;
+  if ($opt eq '\\') {
+      $escape_backslashes = 1;
+  }
+  elsif ($opt eq '/') {
+      $make_forward_slashes = 1;
+  }
+  elsif (substr($opt, 0, 2) eq '-h') {
+      help();
+      exit 0;
+  }
+}
+
 my $path = getcwd;
-$path =~ s/\//\\/g;
+
+if (!$make_forward_slashes) {
+  $path =~ s/\//\\/g;
+}
+if ($escape_backslashes) {
+  $path =~ s/\\/\\\\/g;
+}
+
 
 Win32::Clipboard -> Set($path);
+
+sub help {
+  print '
+  
+    pc
+    pc /     return c:/path/to/file.txt
+    pc \     return c:\\\\path\\\\to\\\\file.txt
+
+  ';
+
+}
