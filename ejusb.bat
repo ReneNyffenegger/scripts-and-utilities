18031694d0d21a11d13e73c75092bed2e4185f30
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-10 06:09:26 +0100

    + ejusb.bat

---
+++ ejusb.bat
@@ -0,0 +1,9 @@
+@rem
+@rem   Eject USB Drive
+@rem
+@rem   Needs RemoveDrive by Uwe Sieber - www.uwe-sieber.de/drivetools_e.html
+@rem
+@rem   Found via http://superuser.com/a/443163/16610
+@rem
+
+RemoveDrive.exe %extern_harddisk_1% -L 
