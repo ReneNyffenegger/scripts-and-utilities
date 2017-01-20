85b51ba1c7839394bc10b45663de5739a27823e2
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-20 20:11:29 +0100

    Start Uniform Server

--- websrv.bat
+++ websrv.bat
@@ -1,1 +1,13 @@
-@%github_root%PerlModules\HTTP\Server\Simple\CGI\webserver.pl
+@rem  --------------------------------------------------------------------------------------
+@rem  --- U  n  i  f  o  r  m    S  e  r  v  e  r                                        ---
+@rem  --- Command line arguments for Uniform Server:                                     ---
+@rem  --- http://www.uniformserver.com/ZeroXI_documentation/command_line_parameters.html ---
+@rem  ---                                                                                ---
+@start    c:\tools\UniServerZ\UniController.exe start_both
+@rem  --------------------------------------------------------------------------------------
+
+
+@rem  --------------------------------------------------------------------------------------
+@rem  --- HTTP::Server::Simple::CGI                                                      ---
+@rem  @%github_root%PerlModules\HTTP\Server\Simple\CGI\webserver.pl
+@rem  --------------------------------------------------------------------------------------
