3e7cce87125ad35a8f510ebcd8713df105e15665
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-09 15:44:36 +0100

    Add --chunk-size --buf-size

--- split-file.pl
+++ split-file.pl
@@ -2,28 +2,37 @@
 use warnings;
 use strict;
 
-my $filename = shift or die;
+use Getopt::Long;
+
+my $chunk_size = 1024*1024;
+my $buf_size   =   10*1024;
+GetOptions (
+  'buf-size:i'    => \$buf_size,
+  'chunk-size:i'  => \$chunk_size
+) or die;
+
+my $filename = shift or die "No filename given";
 
 open (my $in, '<', $filename) or die;
 binmode $in;
 
-my $cnt_out    = 0;
 my $cnt_chunks = 0;
+my $cnt_bufs   = 0;
 
-open (my $out, '>', sprintf("$filename.%03d", $cnt_out)) or die;
+open (my $out, '>', sprintf("$filename.%03d", $cnt_chunks)) or die;
 binmode $out;
 
-while (read($in, my $buf, 10000)) {
+while (read($in, my $buf, ($cnt_bufs+1) * $buf_size < $chunk_size ?  $buf_size : ($cnt_bufs+1)*$buf_size - $chunk_size)) {
 
-    $cnt_chunks ++;
+    $cnt_bufs ++;
     print $out $buf;
 
-    if ($cnt_chunks  == 100) {
-       $cnt_out ++;
-       $cnt_chunks = 0;
+    if ($cnt_bufs * $buf_size > $chunk_size) {
+       $cnt_chunks ++;
+       $cnt_bufs = 0;
        close $out;
 
-       open ($out, '>', sprintf('zefix.db.%003d', $cnt_out)) or die;
+       open ($out, '>', sprintf("$filename.%003d", $cnt_chunks)) or die;
        binmode $out;
 
     }
