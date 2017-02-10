9b3a7cade4365979e8b52023e6f2a9dfb4d2b591
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-02-10 13:30:02 +0100

    -d option

--- split-file.pl
+++ split-file.pl
@@ -6,20 +6,24 @@ use Getopt::Long;
 
 my $chunk_size = 1024*1024;
 my $buf_size   =   10*1024;
+my $dest_dir   = '.';
 GetOptions (
   'buf-size:i'    => \$buf_size,
-  'chunk-size:i'  => \$chunk_size
+  'chunk-size:i'  => \$chunk_size,
+  'dest-dir:s'    => \$dest_dir
 ) or die;
 
 my $filename = shift or die "No filename given";
 
-open (my $in, '<', $filename) or die;
+die unless -d $dest_dir;
+
+open (my $in, '<', $filename) or die "Could not open $filename\n$!";
 binmode $in;
 
 my $cnt_chunks = 0;
 my $cnt_bufs   = 0;
 
-open (my $out, '>', sprintf("$filename.%03d", $cnt_chunks)) or die;
+open (my $out, '>', sprintf("$dest_dir/$filename.%03d", $cnt_chunks)) or die;
 binmode $out;
 
 while (read($in, my $buf, ($cnt_bufs+1) * $buf_size < $chunk_size ?  $buf_size : ($cnt_bufs+1)*$buf_size - $chunk_size)) {
@@ -32,7 +36,7 @@ while (read($in, my $buf, ($cnt_bufs+1) * $buf_size < $chunk_size ?  $buf_size :
        $cnt_bufs = 0;
        close $out;
 
-       open ($out, '>', sprintf("$filename.%003d", $cnt_chunks)) or die;
+       open ($out, '>', sprintf("$dest_dir/$filename.%003d", $cnt_chunks)) or die;
        binmode $out;
 
     }
