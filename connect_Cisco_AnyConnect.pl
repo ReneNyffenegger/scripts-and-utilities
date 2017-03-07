a9d7bf8e6c333d939845f1dce2610b05cf2318fd
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-03-07 07:46:09 +0100

    use verbose

--- connect_Cisco_AnyConnect.pl
+++ connect_Cisco_AnyConnect.pl
@@ -8,6 +8,8 @@ use strict;
 
 use Win32::GuiTest qw(WaitWindowLike FindWindowLike SetForegroundWindow SendKeys WaitWindow GetChildWindows GetChildWindows GetClassName GetWindowText GetWindowRect);
 
+my $verbose = 1;
+
 my $host      = shift;
 die "Indicate a host to connect to" unless $host;
 
@@ -22,29 +24,35 @@ system 'start "" "c:\Program Files\Cisco\Cisco AnyConnect Secure Mobility Client
         WaitWindowLike(undef, $cisco_txt, '#32770', undef, undef, 1, 1);
 my @w = FindWindowLike(undef, $cisco_txt, '#32770', undef, undef, 1);
 
+die unless @w;
+print "Found Window with >$cisco_txt<\n" if $verbose;
+
 die if @w > 1;
 SetForegroundWindow($w[0]);
 
-print "1\n";
 SendKeys('{TAB 2}{ENTER}', 0);
 
 WaitWindow("Cisco AnyConnect \\| $host");
 
-print "2\n";
+print "Window >Cisco AnyConnect<\n" if $verbose;
 SendKeys($password, 0);
 SendKeys('{TAB}{ENTER}', 0);
 
 
 my $wnd_connection_status;
 for my $c (GetChildWindows($w[0])) { #_{
+  printf "Child Window $c, %s\n", GetClassName($c) if $verbose;
   if (GetClassName($c) eq 'Static') {
     my ($l_, $t_, $r_, $b_) = GetWindowRect($c);
+    printf ("  %4d %4d %4d %4d\n", $l_, $t_, $r_, $b_) if $verbose;
     if ($l_ == 610 and $t_ == 423 and $r_ == 907 and $b_ == 449) {
       $wnd_connection_status = $c;
       last;
     }
   }
 } #_}
+
+print "wnd_connection_status = $wnd_connection_status\n" if $verbose;
 
 while (1) { #_{
   last if (GetWindowText($wnd_connection_status) =~ /^Connected to/);
