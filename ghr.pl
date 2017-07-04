38b940d977a0010e52156d47caef76112ebf85c2
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-04 08:16:57 +0200

    about-cpp

--- ghr.pl
+++ ghr.pl
@@ -103,6 +103,7 @@ $repos{'Bash'                               } = {url => 'https://github.com/Rene
 $repos{'boost'                              } = {url => 'https://github.com/ReneNyffenegger/about-boost'                      , dir => $about_dir };
 $repos{'CGI'                                } = {url => 'https://github.com/ReneNyffenegger/about-cgi'                        , dir => $about_dir };
 $repos{'coffeescript'                       } = {url => 'https://github.com/ReneNyffenegger/about-coffeescript'               , dir => $about_dir };
+$repos{'cpp'                                } = {url => 'https://github.com/ReneNyffenegger/about-cpp'                        , dir => $about_dir };
 $repos{'css'                                } = {url => 'https://github.com/ReneNyffenegger/about-css'                        , dir => $about_dir };
 $repos{'cmd.exe'                            } = {url => 'https://github.com/ReneNyffenegger/about-cmd.exe'                    , dir => $about_dir };
 $repos{'dat.GUI'                            } = {url => 'https://github.com/ReneNyffenegger/about-dat.GUI'                    , dir => $about_dir };
