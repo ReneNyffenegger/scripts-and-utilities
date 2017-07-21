108b1741c79ff6aa77008c4e4ac6d4b59a73003e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-21 18:24:22 +0200

    + compile dll

--- ghr.pl
+++ ghr.pl
@@ -190,6 +190,7 @@ $repos{'Chronologie'                        } = {url => 'https://github.com/Rene
 $repos{'Clarify'                            } = {url => 'https://github.com/ReneNyffenegger/Clarify'                            , dir => $github_dir};
 $repos{'cpp-base64'                         } = {url => 'https://github.com/ReneNyffenegger/cpp-base64'                         , dir => $github_dir};
 $repos{'Configure-Windows'                  } = {url => 'https://github.com/ReneNyffenegger/Configure-Windows'                  , dir => $github_dir};
+$repos{'compile-dll'                        } = {url => 'https://github.com/ReneNyffenegger/compile-dll'                        , dir => $github_dir};
 $repos{'crawler'                            } = {url => 'https://github.com/ReneNyffenegger/crawler'                            , dir => $github_dir};
 $repos{'Csound'                             } = {url => 'https://github.com/ReneNyffenegger/Csound'                             , dir => $github_dir};
 $repos{'data-stackexchange'                 } = {url => 'https://github.com/ReneNyffenegger/data-stackexchange'                 , dir => $github_dir};
