e48c59c810957c5730eefb8048c327cb31d88bfe
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-11-06 16:11:07 +0100

    Create highlight.pl

---
+++ highlight.pl
@@ -0,0 +1,15 @@
+/bin/bash: use: command not found
+use warnings;
+use strict;
+
+my $red     = chr(27) . '[1;31m';
+my $yellow  = chr(27) . '[1;33m';
+my $nocolor = chr(27) . '[0m';
+
+while (my $line = <>) {
+
+  $line =~ s/ERROR/${red}$&${nocolor}/g;
+  $line =~ s/WARNING/${yellow}$&${nocolor}/g;
+  print $line;
+
+}
