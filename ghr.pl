9fac92a623881d52f6e040c85fe63d11c7d28e76
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2018-06-01 19:29:50 +0200

    WinAPI-4-VBA

--- ghr.pl
+++ ghr.pl
@@ -266,6 +266,7 @@ $repos{'vim'                                       } = {url => 'https://github.c
 $repos{'Vortraege'                                 } = {url => 'https://github.com/ReneNyffenegger/Vortraege'                               , dir => $github_dir};
 $repos{'WebAutomation'                             } = {url => 'https://github.com/ReneNyffenegger/WebAutomation'                           , dir => $github_dir};
 $repos{'Windows-API'                               } = {url => 'https://github.com/ReneNyffenegger/Windows-API'                             , dir => $github_dir};
+$repos{'WinAPI-4-VBA'                              } = {url => 'https://github.com/ReneNyffenegger/WinAPI-4-VBA'                            , dir => $github_dir};
 $repos{'wfind'                                     } = {url => 'https://github.com/ReneNyffenegger/wfind'                                   , dir => $github_dir};
 $repos{'word-db'                                   } = {url => 'https://github.com/ReneNyffenegger/word-db'                                 , dir => $github_dir};
 $repos{'wordlists.ch'                              } = {url => 'https://github.com/ReneNyffenegger/wordlists.ch'                            , dir => $github_dir};
