96a02349c2de7eaaecc9bdf77e1f7eef19f4b008
René Nyffenegger <mail@renenyffenegger.ch>
2018-12-31 09:02:25 +0100

    + HKCR

--- regat.bat
+++ regat.bat
@@ -2,6 +2,7 @@
 
 @set RegistryKey=%RegistryKey:HKCU=HKEY_CURRENT_USER%
 @set RegistryKey=%RegistryKey:HKLM=HKEY_LOCAL_MACHINE%
+@set RegistryKey=%RegistryKey:HKCR=HKEY_CLASSES_ROOT%
 
 @reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d "%RegistryKey%" /f
 
