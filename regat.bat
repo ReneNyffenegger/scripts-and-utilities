c48be3d374bca4a9a7036992cc49e8c2eabb2b07
René Nyffenegger <mail@renenyffenegger.ch>
2019-01-09 12:56:26 +0100

    check for env

--- regat.bat
+++ regat.bat
@@ -1,9 +1,20 @@
-@set RegistryKey=%*
+@if "%1" == "" (
+  @echo Provide a registry key or env
+  @exit /b
+)
+
+
+@if %1 == env (
+  @set RegistryKey="HKCU\Environment"
+) else (
+  @set RegistryKey=%*
+)
+
 
 @set RegistryKey=%RegistryKey:HKCU=HKEY_CURRENT_USER%
 @set RegistryKey=%RegistryKey:HKLM=HKEY_LOCAL_MACHINE%
 @set RegistryKey=%RegistryKey:HKCR=HKEY_CLASSES_ROOT%
 
-@reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d "%RegistryKey%" /f
+@reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d "%RegistryKey%" /f > nul
 
 @start regedit
