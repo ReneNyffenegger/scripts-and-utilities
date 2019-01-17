3fe9b383fc736f4c768a11102860c9ac0aefa716
René Nyffenegger <mail@renenyffenegger.ch>
2019-01-17 07:44:33 +0100

    taskkill

--- regat.bat
+++ regat.bat
@@ -15,6 +15,11 @@
 @set RegistryKey=%RegistryKey:HKLM=HKEY_LOCAL_MACHINE%
 @set RegistryKey=%RegistryKey:HKCR=HKEY_CLASSES_ROOT%
 
+@rem Kill regedit.exe process if already running.
+@rem If it's not running, it would write an error message. Redirect
+@rem the message to nul
+@taskkill /f /im regedit.exe 2> nul
+
 @reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d "%RegistryKey%" /f > nul
 
 @start regedit
