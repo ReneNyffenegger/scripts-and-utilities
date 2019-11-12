cc8987ce9e6e63682987137cc96aaf4cd0139a55
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-12 20:50:17 +0100

    set-clipboardText  in PowerShell Core

--- profile.ps1
+++ profile.ps1
@@ -10,7 +10,14 @@ function dod { get-childItem | sort-object lastWriteTime }
 # Equivalent of «dir /s /b» in cmd.exe  ( http://stackoverflow.com/a/1479683/180275 )
 function dsb($pattern) { get-childItem -filter $pattern  -recurse -force | select-object -expandProperty fullName }
 
-function pc() { (get-item .).ToString() | set-clipboard }
+if ($psEdition -eq 'Desktop') {
+  function pc() { (get-item .).ToString() | set-clipboard }
+}
+else {
+# clipboardText needs to be installed:
+#    install-module -name ClipboardText
+  function pc() { (get-item .).ToString() | set-clipboardText }
+}
 
 # vi editing mode;
 set-psReadLineOption -editMode vi
