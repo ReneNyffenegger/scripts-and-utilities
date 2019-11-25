862c950f823ae73ebdcca2b2dc68e59b509d93b7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-11-25 07:51:26 +0100

    add function paths()

--- profile.ps1
+++ profile.ps1
@@ -10,6 +10,9 @@ function dod { get-childItem | sort-object lastWriteTime }
 # Equivalent of «dir /s /b» in cmd.exe  ( http://stackoverflow.com/a/1479683/180275 )
 function dsb($pattern) { get-childItem -filter $pattern  -recurse -force | select-object -expandProperty fullName }
 
+# Show individual path-components of the PATH environment variable, each on its own line:
+function paths() { $env:PATH -split ';' }
+
 if ($psEdition -eq 'Desktop') {
   function pc() { (get-item .).ToString() | set-clipboard }
 }
