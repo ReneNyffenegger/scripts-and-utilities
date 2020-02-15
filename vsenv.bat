88f3b7c9a0651ed3bf935102b3e42ee10ddcaf51
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-02-15 10:08:55 +0100

    Add parameter 64

--- vsenv.bat
+++ vsenv.bat
@@ -1,1 +1,28 @@
-@for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do "%%a\Common7\Tools\VsDevCmd.bat"
+@echo off
+
+rem
+rem Uncomment setting VSCMD_DEBUG to enable debugging to output
+rem
+rem set VSCMD_DEBUG=3
+
+rem
+rem   Determine path to VsDevCmd.bat
+rem
+for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do set VsDevCmd_Path=%%a\Common7\Tools\VsDevCmd.bat
+echo Path is %VsDevCmd_Path%
+
+
+if [%1] equ [64] (
+
+   echo -arch=amd64
+  "%VsDevCmd_Path%" -arch=amd64
+
+) else (
+
+  echo Don't use -arch
+  "%VsDevCmd_Path%"
+
+)
+
+rem set VSCMD_DEBUG=
+set VsDevCmd_Path=
