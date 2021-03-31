ebc1cd8e70879f867dc3d75f76a76d18c01045d8
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-03-31 23:29:21 +0200

    comment about AC

--- notify_me_at.bat
+++ notify_me_at.bat
@@ -1,17 +1,22 @@
-@set year=%1
-@set month=%2
-@set day=%3
-@set hour=%4
-@set minute=%5
+@echo off
 
-@set message=%hour%:%minute% - %*
+rem 
+rem Does not work if notebook is running on batteries!
+rem
 
-@set taskname=NotifyMeAt%year%%month%%day%%year%%hour%%minute%
+set year=%1
+set month=%2
+set day=%3
+set hour=%4
+set minute=%5
 
-@rem
-@rem  TODO
-@rem    The task removes itself after running once. Maybe, /Z could
-@rem    be used to delete the task after running once.
-@rem
-@rem @schtasks /create /SC once /ST %hour%:%minute%:00 /TN %taskname% /SD %day%/%month%/%year% /TR "cmd /c msg * %message% && schtasks /delete /tn %taskname% /f"
-     @schtasks /create /SC once /ST %hour%:%minute%:00 /TN %taskname% /SD %year%/%month%/%day% /TR "cmd /c c:\users\rene\x.bat "
+set message=%hour%:%minute% - %*
+
+set taskname=NotifyMeAt%year%%month%%day%%year%%hour%%minute%
+
+rem
+rem  TODO
+rem    The task removes itself after running once. Maybe, /Z could
+rem    be used to delete the task after running once.
+rem
+    @schtasks /create /SC once /ST %hour%:%minute%:00 /TN %taskname% /SD %day%/%month%/%year% /TR "cmd /c msg * %message% && schtasks /delete /tn %taskname% /f"
