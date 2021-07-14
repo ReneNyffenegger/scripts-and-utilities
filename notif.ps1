352cb6e1f487d59af189bb6e2b767f2847ca0e1d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-07-14 09:42:15 +0200

    V0.4

--- notif.ps1
+++ notif.ps1
@@ -1,10 +1,36 @@
+#
+#  V0.4
+#
+[cmdletBinding(defaultParameterSetName = 'set')]
 param (
-   [switch]          $g,
-   [string]          $when,
-   [string]          $msg
+
+   [parameter(
+        mandatory = $false,
+        parameterSetName = '-g')]
+       [switch]                       $g,
+
+   [parameter(
+        mandatory        = $true,
+        position         =  1,
+        parameterSetName = 'set')]
+       [string]                       $when,
+
+   [parameter(
+        mandatory        = $true,
+        position         =  2,
+        parameterSetName = 'set')]
+       [string]                       $msg
 )
 
 set-strictMode -version latest
+
+if ($g) {
+   $actions = get-scheduledTask | where-object taskname -match 'notif_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}' # | select-object actions
+   foreach ($action in $actions) {
+      "$($action.taskName)  $($action.actions[0].arguments)"
+   }
+   return
+}
 
 if ($when.substring(0, 1) -eq '+') {
   [timespan] $span = $when.substring(1)
@@ -12,17 +38,6 @@ if ($when.substring(0, 1) -eq '+') {
 }
 else {
   [datetime] $dtWhen = $when
-}
-
-
-if ($g) {
-   $actions = get-scheduledTask | where-object taskname -match 'notif_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}' # | select-object actions
-   foreach ($action in $actions) {
-   #  $action | get-member
-     "$($action.taskName)  $($action.actions[0].arguments)"
-   #  $action.triggers[0]
-   }
-   return
 }
 
 $trg = new-scheduledTaskTrigger   -once -at $dtWhen
@@ -35,9 +50,9 @@ $set = new-scheduledTaskSettingsSet  `
 
 $act = new-scheduledTaskAction -execute "cmd" -argument "/c msg $env:username $msg"
 
-$tsk = register-scheduledTask                              `
-   -force                                                  `
+$null = register-scheduledTask                               `
+   -force                                                    `
    -taskName "notif_$($dtWhen.toString('yyyy-MM-dd_HH-mm'))" `
-   -Trigger    $trg                                        `
-   -action     $act                                        `
+   -Trigger    $trg                                          `
+   -action     $act                                          `
    -settings   $set
