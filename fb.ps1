39caa26a59f752c634ea8662d043e9b43d151c0e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-11-03 06:13:26 +0100

    V.3

--- fb.ps1
+++ fb.ps1
@@ -1,6 +1,6 @@
 # fb = Find below
 #
-# V.2
+# V.3
 #
 # use fullName in output to emulate "dir /b /s"
 #
@@ -8,5 +8,5 @@ param (
    [string] $wildcard
 )
 
-get-childItem -errorAction silentlyContinue -recurse -filter $wildcard |
+get-childItem -force -errorAction silentlyContinue -recurse -filter $wildcard |
 select-object -expandProperty fullName  # use -expandProperty so that output is not truncated, see https://stackoverflow.com/q/70694197/180275
