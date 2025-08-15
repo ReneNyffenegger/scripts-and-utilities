d2982a9349f2bd0a614e67c66bdab5bf6db6ce35
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2025-08-15 09:02:24 +0200

    V.2: Show full dimesions and workarea size

--- screens.ps1
+++ screens.ps1
@@ -1,6 +1,12 @@
+# V.2
 add-type -assemblyName System.Windows.Forms
+
+''
+'Monitor       Dimension  Work Area         Offset'
+'-------       ---------  ---------    -----------'
 
 foreach ($screen in [System.Windows.Forms.Screen]::AllScreens) {
     $wa = $screen.WorkingArea
-   '{0,-18}  {1,4}x{2,4} @ {3,4},{4,4}' -f $screen.DeviceName, $wa.Width, $wa.Height, $wa.X, $wa.Y
+   '{0:-18}  {1,4}x{2,4}  {3,4}x{4,4}  @ {5,5},{6,5}' -f $screen.DeviceName, $screen.Bounds.Width, $Screen.Bounds.Height, $wa.Width, $wa.Height, $wa.X, $wa.Y
 }
+''
